43facd4e57b0476d9cf8ee9f1b72a94cc2baa09a
Oliver Both Ipsen <obip38712@edu.ucl.dk>
2021-11-15 08:55:44 +0100

    For int Figur

--- OpgaveForm/Program.cs
+++ OpgaveForm/Program.cs
@@ -10,10 +10,15 @@ namespace OpgaveForm
             Cirkel C = new Cirkel(2);
             C.Areal();
             Console.WriteLine();
-            Trekant T = new Trekant(3,5);
+            Trekant T = new Trekant(3, 5);
             T.Areal();
 
-
+            Figur[] figur = new Figur[2];
+            figur[0] = C;
+            figur[1] = T;
+            for (int i = 0; i < figur.Length; i++)
+                figur[i].Areal();
+                
 
         }
     }
